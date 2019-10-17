40ba12fb8b72f0924825d3013e6451f366b029ad
August <august.hjertstedt@gmail.com>
2019-10-17 19:28:36 +0200

    check input på namn och tal

--- main.js
+++ main.js
@@ -41,7 +41,10 @@ class Question{
 var quiz = new Quiz();
 function hideMain(){
     var numberCheck = document.getElementById("antal").value;
-    console.log(numberCheck);
+    if(document.getElementById("namn").value === ""){
+        alert("Fyll i ett namn");
+        return;
+    }
     if(isNaN(numberCheck) == true || numberCheck > 10 || numberCheck <= 0){
         alert("Fyll i ett tal mellan 1 - 10");
         return;
